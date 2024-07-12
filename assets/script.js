d4977723a2020925c136c34d99b00bbd974891e7
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2024-07-12 23:34:56 +0200

    lazy loading test 1

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,26 @@ window.addEventListener("scroll", function () {
     arrowTop.style.display = "none";
   }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  function lazyLoadBackgroundImages() {
+      const lazyBackgrounds = document.querySelectorAll('.lazy-background');
+
+      const observer = new IntersectionObserver(entries => {
+          entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                  const bgImage = entry.target.getAttribute('data-bg');
+                  entry.target.style.backgroundImage = `url(${bgImage})`;
+                  observer.unobserve(entry.target);
+              }
+          });
+      });
+
+      lazyBackgrounds.forEach(background => {
+          observer.observe(background);
+      });
+  }
+
+  lazyLoadBackgroundImages();
+});
